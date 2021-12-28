4c1e9cf5ba869f848171b453945bd7133c0de960
nosthrillz <ilie06@gmail.com>
2021-12-28 23:42:47 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default function Header() {
+  return (
+    <Wrapper>
+      <h1>eetetete</h1>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div``;
